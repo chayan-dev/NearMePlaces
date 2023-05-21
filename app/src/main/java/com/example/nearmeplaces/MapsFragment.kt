fec542cf0a421a974292e9dd7ec71abffc5f9e3e
Chayan Das <chayandas0308@gmail.com>
2023-05-21 11:55:01 +0530

    runtime permission for location

--- app/src/main/java/com/example/nearmeplaces/MapsFragment.kt
+++ app/src/main/java/com/example/nearmeplaces/MapsFragment.kt
@@ -1,10 +1,15 @@
 package com.example.nearmeplaces
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,6 +20,14 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
   private lateinit var mMap: GoogleMap
+  private val requestPermissionLauncher =
+    registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+      if (isGranted) {
+        checkLocationPermission()
+      } else {
+        //TODO: show toast
+      }
+    }
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -35,12 +48,27 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
   override fun onMapReady(googleMap: GoogleMap) {
     mMap = googleMap
 
+    checkLocationPermission()
+
     // Add a marker in Sydney and move the camera
     val sydney = LatLng(-34.0, 151.0)
     mMap.addMarker(MarkerOptions()
       .position(sydney)
       .title("Marker in Sydney"))
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+  }
+
+  private fun checkLocationPermission() {
+    if (ContextCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+      ) != PackageManager.PERMISSION_GRANTED
+    ) {
+      // Permission is not granted, request it
+      requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+      return
+    }
+    Log.d("MapsFragment","having loc permission")
   }
 
 }
